e21b5637167299f25a2b39e00a722d9fb51540fa
YukiKuroshima <yukikuro1993@gmail.com>
2017-10-22 00:41:43 -0700

    :tada: Add jwt

--- letitfly/app/__init__.py
+++ letitfly/app/__init__.py
@@ -1,9 +1,8 @@
 from flask_api import FlaskAPI, status
 from flask_sqlalchemy import SQLAlchemy
-from flask import Blueprint, render_template, abort, request, jsonify  # Blueprints
+from flask import Blueprint, render_template, abort, request, make_response, jsonify  # Blueprints
 
 # For route
-from flask_jwt import JWT, jwt_required, current_identity
 from werkzeug.security import safe_str_cmp
 from sqlalchemy import exc
 import sys
@@ -21,10 +20,50 @@ def create_app(config_name):
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
-    from app.models import drives_model, users_model
+    from app.models.drives_model import Rides
+    from app.models.users_model import Users
 
-    # POST /auth
-    # Flask-JWT auto generate /auth 
+    # POST /authenticate
+    """
+    Sample
+    POST /authenticate
+    Host: localhost:5000
+    Content-Type: application/json
+
+    {
+        "username": "joe",
+        "password": "pass"
+    }
+    """
+    @app.route('/auth', methods=['POST'])
+    def authenticate():
+        try:
+            # Get the user object using their email (unique to every user)
+            user = Users.query.filter_by(username=request.data['username']).first()
+
+            # Try to authenticate the found user using their password
+            if user and user.password_is_valid(request.data['password']):
+                # Generate the access token. This will be used as the authorization header
+                access_token = user.generate_token(user.user_id)
+                if access_token:
+                    response = {
+                            'message': 'You logged in successfully.',
+                            'access_token': access_token.decode()
+                            }
+                    return make_response(jsonify(response)), 200
+            else:
+                # User does not exist. Therefore, we return an error message
+                response = {
+                        'message': 'Invalid email or password, Please try again'
+                        }
+                return make_response(jsonify(response)), 401
+
+        except Exception as e:
+            # Create a response containing an string error message
+            response = {'message': str(e)}
+            # Return a server error using the HTTP Error Code 500 (Internal Server Error)
+            return make_response(jsonify(response)), 500
+
     # POST /register
     """
     Sample json data
@@ -42,7 +81,7 @@ def create_app(config_name):
     def register():
         try:
             user_data = request.data
-            temp_user = users_model.Users(
+            temp_user = Users(
                     first_name=user_data.get('first_name'),
                     last_name=user_data.get('last_name'),
                     credit_card=user_data.get('credit_card'),
@@ -73,7 +112,7 @@ def create_app(config_name):
 
     @app.route("/test", methods=['GET'])
     def hello():
-        temp_user = users_model.Users(
+        temp_user = Users(
                 first_name='Test',
                 last_name='Test Last',
                 credit_card=1234,
@@ -83,7 +122,7 @@ def create_app(config_name):
                 password='test'
                 )
         temp_user.save()
-        temp_user1 = users_model.Users(
+        temp_user1 = Users(
                 first_name='Test',
                 last_name='Test Last',
                 credit_card=1234,
@@ -93,7 +132,7 @@ def create_app(config_name):
                 password='test'
                 )
         temp_user1.save()
-        temp_ride = drives_model.Rides(
+        temp_ride = Rides(
                 customer=temp_user,
                 driver=temp_user1,
                 start_location='Test Last',
--- letitfly/app/models/users_model.py
+++ letitfly/app/models/users_model.py
@@ -1,4 +1,8 @@
 from app import db
+from flask import current_app
+import jwt
+from datetime import datetime, timedelta
+from werkzeug.security import safe_str_cmp
 
 
 class Users(db.Model):
@@ -39,3 +43,42 @@ class Users(db.Model):
     def __repr__(self):
         """Represent user by name"""
         return "{} {}".format(self.first_name, self.last_name)
+    
+    def password_is_valid(self, password):
+        """
+        Checks the password against it's hash to validates the user's password
+        """
+        return safe_str_cmp(self.password.encode('utf-8'), password.encode('utf-8'))
+
+    def generate_token(self, user_id):
+        """Generates the access token to be used as the Authorization header"""
+
+        try:
+            # set up a payload with an expiration time
+            payload = {
+                'exp': datetime.utcnow() + timedelta(minutes=5),
+                'iat': datetime.utcnow(),
+                'sub': user_id
+            }
+            # create the byte string token using the payload and the SECRET key
+            jwt_string = jwt.encode(
+                payload,
+                current_app.config.get('SECRET'),
+                algorithm='HS256'
+            )
+            return jwt_string
+
+        except Exception as e:
+            # return an error in string format if an exception occurs
+            return str(e)
+
+    @staticmethod
+    def decode_token(token):
+        """Decode the access token from the Authorization header."""
+        try:
+            payload = jwt.decode(token, current_app.config.get('SECRET'))
+            return payload['sub']
+        except jwt.ExpiredSignatureError:
+            return "Expired token. Please log in to get a new token"
+        except jwt.InvalidTokenError:
+            return "Invalid token. Please register or login"
