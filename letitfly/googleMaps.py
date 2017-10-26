4c152f78272159e02f55aee35d92a73a77f5ed8f
domuchu <lau.marcus76@gmail.com>
2017-10-26 02:33:04 -0700

    Create googleMaps.py

---
+++ letitfly/googleMaps.py
@@ -0,0 +1,66 @@
+
+from flask import Flask, render_template
+from flask_googlemaps import GoogleMaps
+from flask_googlemaps import Map, icons
+import webbrowser
+
+app = Flask(__name__, template_folder="templates")
+
+# you can set key as config
+app.config['GOOGLEMAPS_KEY'] = "AIzaSyAck9gozAKmXvYT3KXbkvYqV8mU9Vs7Qdk"
+
+# you can also pass key here
+GoogleMaps(app, key="AIzaSyAck9gozAKmXvYT3KXbkvYqV8mU9Vs7Qdk")
+
+
+@app.route('/', methods=["GET"])
+def fullmap():
+    fullmap = Map(
+        identifier="fullmap",
+        varname="fullmap",
+        style=(
+            "height:100%;"
+            "width:100%;"
+            "top:0;"
+            "left:0;"
+            "position:absolute;"
+            "z-index:200;"
+        ),
+        # hardcoded coordinates to SJSU for now
+        lat=37.3352,
+        lng=-121.8811,
+        markers=[
+            {
+                'icon': '//maps.google.com/mapfiles/ms/icons/green-dot.png',
+                'lat': 37.3352,
+                'lng': -121.8811,
+                'infobox': "Hello I am <b style='color:green;'>HELLO WORLD GREEN</b>!"
+            },
+            {
+                'icon': '//maps.google.com/mapfiles/ms/icons/blue-dot.png',
+                'lat': 37.4300,
+                'lng': -122.1400,
+                'infobox': "Hello I am <b style='color:blue;'>HELLO WORLD BLUE</b>!"
+            },
+            {
+                'icon': icons.dots.yellow,
+                'title': 'Click Here',
+                'lat': 37.4500,
+                'lng': -122.1350,
+                'infobox': (
+                    "Hello I am <b style='color:#ffcc00;'>YELLOW</b>!"
+                    "<h2>It is HTML title</h2>"
+                    "<img src='//placehold.it/50'>"
+                    "<br>Images allowed!"
+                )
+            }
+        ],
+        # maptype = "TERRAIN",
+        # zoom="5"
+    )
+    return render_template('directory.html', fullmap=fullmap)
+
+
+if __name__ == "__main__":
+    #webbrowser.open('http://127.0.0.1:5000')
+    app.run(debug=True, use_reloader=True)
