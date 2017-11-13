2b258536a66ec65ecb64f4fed645210860e46fb3
YukiKuroshima <yukikuro1993@gmail.com>
2017-11-12 17:42:28 -0800

    :tada: Set up port and host for heroku

--- letitfly/run.py
+++ letitfly/run.py
@@ -7,4 +7,6 @@ config_name = os.getenv('APP_SETTINGS')  # config_name = "development"
 app = create_app(config_name)
 
 if __name__ == '__main__':
-    app.run()
+    # Heroku needs this
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
